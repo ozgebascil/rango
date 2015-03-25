df3783dc55a124ac134fab1d5cad3aa298e379af
Ozge Bascil <ozge14@gmail.com>
2015-03-25 13:33:34 +0000

    Added new category

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rango.models import Category 
-
+from rango.models import Category
+from rango.models import Page
 
 
 def index(request):
@@ -15,3 +15,17 @@ def index(request):
     # Render the response and send it back!
     return render(request, 'rango/index.html', context_dict)
 
+def category(request, category_name_slug):
+    context_dict = { }
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        pass
+
+    return render(request, 'rango/category.html', context_dict)
