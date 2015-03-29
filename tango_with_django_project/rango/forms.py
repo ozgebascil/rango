fa469fbfcf6c9a52890fd54a5f1eb541a3c474ca
Ozge Bascil <ozge14@gmail.com>
2015-03-29 21:48:39 +0100

    Created the user form and user profile form

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -1,12 +1,13 @@
 from django import forms
-from django.models import Page, Category
+from django.contrib.auth.models import User
+from rango.models import Page, Category, UserProfile
 
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128, help_text='Please enter the category name.')
-    views = form.IntegerField(widget=forms.HiddenInput(), initial=0)
-    likes = form.IntegerField(widget=forms.HiddenInput(), initial=0)
-    slug = form.CharField(widget=forms.HiddenInput(), required=False)
+    name = forms.CharField(max_length=128, help_text="Please enter the category name.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
 
 
     class Meta:
@@ -14,13 +15,40 @@ class CategoryForm(forms.ModelForm):
         fields = ('name',)
 
 
-class PageForm(form.ModelForm):
-    title = forms.CharField(max_length=128, help_text='Please enter the title of the page.')
-    url = form.URLField(max_length=200, help_text='Please enter the URL of the page.')
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
+    url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
 
     class Meta:
         model = Page
         exclude = ('category',)
+
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        if url and not url.startwith('http://'):
+            url = 'http://' + url
+            cleaned_data['url'] = url
+
+        return cleaned_data
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture')
+
+
 
