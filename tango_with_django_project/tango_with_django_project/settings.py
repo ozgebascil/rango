3b4d84764378d0151d57fdddc4d85d538142cd46
Ozge Bascil <ozge14@gmail.com>
2015-04-10 17:44:21 +0100

    Added the registration package

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -104,4 +105,8 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-LOGIN_URL = '/rango/login/'
+REGISTRATION_OPEN = True 
+ACCOUNT_ACTIVATION_DAYS = 7 
+REGISTRATION_AUTO_LOGIN = True 
+LOGIN_REDIRECT_URL = '/rango/' 
+LOGIN_URL = '/accounts/login/' 
